8b507c5e4086cb739ebe3e9ff0d79eb4592e6799
Nicole Garcia <ngarcia201@gmail.com>
2022-09-05 00:38:25 -0700

    intial animation test

---
+++ README.md
@@ -0,0 +1,5 @@
+# Background
+
+My intro to HTML5 games!
+I've always been an avid gamer. Flash (before it reached it's end of life) based games like coolmathgames and armorgames in computer lab time was something I always looked forward too as a kid. I thought it was time to dabble in game devlopement but I felt like jumping into Unity would be a big leap and that's where HTML5 comes to rescue! I think this is perfect meduim to learn game dev concepts and get up and running quickly.
+
--- index.js
+++ index.js
@@ -6,3 +6,39 @@ canvas.height = 576;
 
 c.fillRect(0,0, canvas.width, canvas.height);
 
+class Sprite{
+    constructor({position, velocity}){
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw(){ // draw out sprite
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50,150);
+    }
+
+    update(){
+        this.draw();
+        this.position.y += 10
+    }
+}
+
+const player1 = new Sprite(
+    {position: {x:0 , y:0}, velocity:{x:0 , y:0}} 
+);
+
+const player2 = new Sprite(
+    { position: {x:800 , y:0}, velocity:{x:0 , y:0}}
+);
+
+
+
+
+// intial sprite set up ^^
+function animate(){
+    window.requestAnimationFrame(animate);
+    player1.update();
+    player2.update();
+};
+
+animate();
